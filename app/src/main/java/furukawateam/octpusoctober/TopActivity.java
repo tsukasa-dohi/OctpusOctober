de00f007989cadcdf73419cbd97a6fce2975d5a5
tsukasa1124 <tsukasa.dohi25@gmail.com>
2016-06-30 11:36:46 +0900

    include実装２

--- app/src/main/java/furukawateam/octpusoctober/TopActivity.java
+++ app/src/main/java/furukawateam/octpusoctober/TopActivity.java
@@ -5,9 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class TopActivity extends AppCompatActivity {
 
@@ -24,6 +25,39 @@ public class TopActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button btn1 = (Button)findViewById(R.id.include_view_btn).findViewById(R.id.button1);
+        Button btn2 = (Button)findViewById(R.id.include_view_btn).findViewById(R.id.button2);
+        Button btn3 = (Button)findViewById(R.id.include_view_btn).findViewById(R.id.button3);
+        Button btn4 = (Button)findViewById(R.id.include_view_btn).findViewById(R.id.button4);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
