c5f6f48ab5cd57e01d8f2b3529c2ccfde5e21966
tsukasa1124 <tsukasa.dohi25@gmail.com>
2016-07-01 10:06:35 +0900

    include実装２

--- app/src/main/java/furukawateam/octpusoctober/TopActivity.java
+++ app/src/main/java/furukawateam/octpusoctober/TopActivity.java
@@ -1,8 +1,6 @@
 package furukawateam.octpusoctober;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -18,15 +16,6 @@ public class TopActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Button btn1 = (Button)findViewById(R.id.include_view_btn).findViewById(R.id.button1);
         Button btn2 = (Button)findViewById(R.id.include_view_btn).findViewById(R.id.button2);
