337096b11dafead49aae0c94b4c6edccec6bb35e
tsukasa1124 <tsukasa.dohi25@gmail.com>
2016-07-01 10:29:43 +0900

    include実装２

--- app/src/main/java/furukawateam/octpusoctober/AllTakoyakiActivity.java
+++ app/src/main/java/furukawateam/octpusoctober/AllTakoyakiActivity.java
@@ -4,20 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class TopActivity extends AppCompatActivity {
+public class AllTakoyakiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_top);
+        setContentView(R.layout.activity_all_takoyaki);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         //フッターButtonのクリックイベント
         Button btn1 = (Button)findViewById(R.id.include_view_btn).findViewById(R.id.button1);
         Button btn2 = (Button)findViewById(R.id.include_view_btn).findViewById(R.id.button2);
@@ -27,22 +24,22 @@ public class TopActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(AllTakoyakiActivity.this, TopActivity.class);
+                startActivity(intent);
             }
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TopActivity.this,AllTakoyakiActivity.class);
-                startActivity(intent);
+
             }
         });
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TopActivity.this,MapActivity.class);
+                Intent intent = new Intent(AllTakoyakiActivity.this, MapActivity.class);
                 startActivity(intent);
             }
         });
@@ -55,25 +52,4 @@ public class TopActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_top, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
